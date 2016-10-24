b406ff9ca01b5e3489bfdb929e75f5a638ca7985
ramonornela <ramon.ornela@gmail.com>
2016-10-24 14:13:50 -0200

    refactor(http): simplificando chamada para o modulo

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Http as HttpAngular, BrowserXhr, ResponseOptions, XSRFStrategy, ConnectionBackend } from '@angular/http';
 import { UrlResolverModule } from '@ramonornela/url-resolver';
 import { xhrBackendFactory, HttpEvents } from './backend/xhr_backend';
@@ -15,43 +15,21 @@ import {
 @NgModule({
   imports: [
     UrlResolverModule
+  ],
+  providers: [
+    HttpEvents,
+    {
+      provide: ConnectionBackend,
+      useFactory: xhrBackendFactory,
+      deps: [ BrowserXhr, ResponseOptions, XSRFStrategy, HttpEvents ]
+    },
+    HttpAngular,
+    { provide: ThrowExceptionStatusToken, useValue: null },
+    { provide: ParseResponseToken, useClass: ThrowExceptionStatus, deps: [ ThrowExceptionStatusToken ], multi: true },
+    { provide: Plugins, useClass: Plugins, deps: [ HttpPluginsToken ] },
+    Http,
+    { provide: HttpPluginsToken, useClass: ParseResponsePlugin, deps: [ ParseResponseToken ], multi: true }
   ]
 })
 export class HttpModule {
-  static initialize(plugins?: any): ModuleWithProviders {
-
-    plugins = plugins || [];
-    plugins.unshift([ ParseResponsePlugin, ParseResponseToken ]);
-
-    let pluginsProviders = [];
-
-    // adicionando array de plugins
-    for (let plugin of plugins) {
-      let deps: any = [];
-
-      if (plugin.constructor.name === 'Array') {
-        deps = plugin;
-        plugin = plugin.shift();
-      }
-      pluginsProviders.push({provide: HttpPluginsToken, useClass: plugin, deps: deps, multi: true});
-    }
-
-    return {
-      ngModule: HttpModule,
-      providers: [
-        HttpEvents,
-        {
-          provide: ConnectionBackend,
-          useFactory: xhrBackendFactory,
-          deps: [ BrowserXhr, ResponseOptions, XSRFStrategy, HttpEvents ]
-        },
-        HttpAngular,
-        pluginsProviders,
-        { provide: ThrowExceptionStatusToken, useValue: null },
-        { provide: ParseResponseToken, useClass: ThrowExceptionStatus, deps: [ ThrowExceptionStatusToken ], multi: true },
-        { provide: Plugins, useClass: Plugins, deps: [ HttpPluginsToken ] },
-        Http
-      ]
-    };
-  }
 }
