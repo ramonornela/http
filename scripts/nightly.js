b1270478a40e04ea8a73310d59b1b124102273f8
ramonornela <ramon.ornela@gmail.com>
2016-11-18 21:53:45 -0200

    chore(npm): add scripts geration tag nightly npm

---
+++ scripts/nightly.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+var fluid = require('fluid-publish');
+fluid.dev(false, {devTag: 'nightly'});
--- scripts/package.js
+++ scripts/package.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var fs = require('fs');
 var process = require('process');
 var path = require('path');
