23f496b1b8c015d2eea1e51b859c4d62861b95af
ramonornela <ramon.ornela@gmail.com>
2016-11-18 23:46:35 -0200

    chore(npm): delete prePackage nightly

--- scripts/nightly.js
+++ scripts/nightly.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 
-var prePackage = require('./package');
 var fluid = require('fluid-publish');
-prePackage.prepare();
 fluid.dev(false, {devTag: 'nightly'});
-prePackage.restore();
